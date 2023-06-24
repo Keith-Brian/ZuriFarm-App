507b484736bdb75d35a02226022a97c26e857b11
keith-brian <chiefdeveloper254@gmail.com>
2023-06-24 20:40:38 +0300

    Implemented password & email validator

--- app/src/main/java/com/DevKB/myzuri/ui/auth/SigningActivity.kt
+++ app/src/main/java/com/DevKB/myzuri/ui/auth/SigningActivity.kt
@@ -3,12 +3,15 @@ package com.DevKB.myzuri.ui.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import androidx.lifecycle.ViewModelProvider
 import com.DevKB.myzuri.data.viewModel.AuthViewModel
 import com.DevKB.myzuri.databinding.ActivitySigninBinding
 import com.DevKB.myzuri.ui.dashboard.DashboardActivity
 import com.DevKB.myzuri.utils.ViewUtils.Extension.toast
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 
 class SigningActivity : AppCompatActivity() {
     private lateinit var loginBinding: ActivitySigninBinding
@@ -26,22 +29,46 @@ class SigningActivity : AppCompatActivity() {
             finish()
         }
 
+
+        fun validateCredentials(email: String, password: String): Boolean {
+
+            if (email.isEmpty()){
+                loginBinding.loginEmail.error = "Email cannot be blank!"
+                return false
+            }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                loginBinding.loginEmail.error = "Invalid Password"
+                return false
+            }else if(password.isEmpty()){
+                loginBinding.loginPass.error = "Password Cannot be blank!"
+                return true
+            }else{
+                loginBinding.loginEmail.error = null
+                loginBinding.loginPass.error = null
+                return true
+            }
+        }
+
         loginBinding.btnLogin.setOnClickListener {
 
-            authViewModel.email = loginBinding.loginEmail.text.toString()
-            authViewModel.password = loginBinding.loginPass.text.toString()
 
-            authViewModel.login(authViewModel.email!!,authViewModel.password!!)
-                .addOnCompleteListener(OnCompleteListener {
-                    if(it.isSuccessful){
-                        toast("Login Successful")
-                        val intent = Intent(this, DashboardActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        toast("Login Failed!")
-                    }
-                })
+            if (validateCredentials(loginBinding.loginEmail.text.toString(),loginBinding.loginPass.text.toString())) {
+
+                authViewModel.email = loginBinding.loginEmail.text.toString()
+                authViewModel.password = loginBinding.loginPass.text.toString()
+
+                authViewModel.login(authViewModel.email!!, authViewModel.password!!)
+                    .addOnCompleteListener(OnCompleteListener {
+                        if (it.isSuccessful) {
+                            toast("Login Successful")
+                            val intent = Intent(this, DashboardActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            toast("Login Failed!")
+                        }
+                    })
+
+            }
         }
     }
 }
--- app/src/main/java/com/DevKB/myzuri/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/DevKB/myzuri/ui/auth/SignupActivity.kt
@@ -4,15 +4,32 @@ package com.DevKB.myzuri.ui.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PatternMatcher
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Patterns
 import androidx.lifecycle.ViewModelProvider
 import com.DevKB.myzuri.data.viewModel.AuthViewModel
 import com.DevKB.myzuri.databinding.ActivitySignupBinding
 import com.DevKB.myzuri.ui.dashboard.DashboardActivity
 import com.DevKB.myzuri.utils.ViewUtils.Extension.toast
 import com.google.android.gms.tasks.OnCompleteListener
+import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var registerBinding: ActivitySignupBinding
+
+    private val PASSWORD_PATTERN: Pattern = Pattern.compile(
+        "^" +
+                "(?=.*[0-9])" +         //at least 1 digit
+                "(?=.*[a-z])" +         //at least 1 lower case letter
+                "(?=.*[A-Z])" +         //at least 1 upper case letter
+                //"(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{8,}" +               //at least 4 characters
+                "$"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerBinding = ActivitySignupBinding.inflate(layoutInflater)
@@ -27,21 +44,55 @@ class SignupActivity : AppCompatActivity() {
             finish()
         }
 
-        registerBinding.btnRegister.setOnClickListener {
-            authViewModel.email = registerBinding.regEmail.text.toString()
-            authViewModel.password = registerBinding.regPass.text.toString()
+        fun validateEmail(email: String): Boolean {
 
-            authViewModel.register(authViewModel.email!!, authViewModel.password!!)
-                .addOnCompleteListener(OnCompleteListener {
-                    if(it.isSuccessful){
-                        toast("Registration Successful!")
-                        val intent = Intent(this, DashboardActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        toast("Registration Failed!")
-                    }
-                })
+            if (email.isEmpty()){
+                registerBinding.regEmail.error = "Email cannot be blank!"
+                return false
+            }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                registerBinding.regEmail.error = "Invalid Password"
+                return false
+            }else{
+                registerBinding.regEmail.error = null
+                return true
+            }
+        }
+
+
+
+        fun validatePassword(password: String): Boolean{
+            if (password.isEmpty()){
+                registerBinding.regPass.error = "Password cannot be blank!"
+                return false
+            }else if (!PASSWORD_PATTERN.matcher(password).matches()){
+                registerBinding.regPass.error = "Password is weak"
+               return false
+            }else{
+                registerBinding.regPass.error = null
+                return true
+            }
+        }
+
+        registerBinding.btnRegister.setOnClickListener {
+
+            if(validateEmail(registerBinding.regEmail.text.toString()) && validatePassword(registerBinding.regPass.text.toString())){
+
+                authViewModel.email = registerBinding.regEmail.text.toString()
+                authViewModel.password = registerBinding.regPass.text.toString()
+
+                authViewModel.register(authViewModel.email!!, authViewModel.password!!)
+                    .addOnCompleteListener(OnCompleteListener {
+                        if(it.isSuccessful){
+                            toast("Registration Successful!")
+                            val intent = Intent(this, DashboardActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            toast("Registration Failed!")
+                        }
+                    })
+            }
+
         }
     }
 }
